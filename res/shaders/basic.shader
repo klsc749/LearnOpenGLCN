98355f706f1e75dab3ca81ca89c17c9830032dc0
给贝拉嘉晚饭 <79397301+klsc749@users.noreply.github.com>
2022-02-27 15:02:19 +0800

    Exchange two textures

--- res/shaders/basic.shader
+++ res/shaders/basic.shader
@@ -1,23 +1,32 @@
 #shader vertex
 #version 330 core
-layout(location = 0) in vec3 aPos;   // 位置变量的属性位置值为 0 
-layout(location = 1) in vec3 aColor; // 颜色变量的属性位置值为 1
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
 
-out vec3 ourColor; // 向片段着色器输出一个颜色
+out vec3 ourColor;
+out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
-    ourColor = aColor; // 将ourColor设置为我们从顶点数据那里得到的输入颜色
+	gl_Position = vec4(aPos, 1.0);
+	ourColor = aColor;
+	TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
 
 
 #shader fragment
 #version 330 core
 out vec4 FragColor;
+
 in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float mixVal;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixVal);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(640, 480, "Hello OpenGL", NULL, NULL);
-
+	window = glfwCreateWindow(1200, 900, "Hello OpenGL", NULL, NULL);
+	glfwSwapInterval(2);
 
 	if (!window)
 	{
@@ -35,24 +35,72 @@ int main()
 	}
 
 	float vertices[] = {
-		// positions         // colors
-		 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // top left
-		 0.5f,  -0.5f, 0.0f,  0.0f, 0.0f, 0.0f   // top right
+		//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+			 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+			 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
 	};
 
 	unsigned int indices[] = {
-		0, 1, 2,
-		0, 1, 3
+		0, 1, 3,
+		1, 2, 3
 	};
+
+
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannerls;
 	unsigned char* data = stbi_load("res/images/container.jpg", &width, &height, &nrChannerls, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Fail to read image" << std::endl;
+	}
 
 
-	Shader program("res/shaders/basic.shader");
-	program.Use();
+	stbi_image_free(data);
+
+
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	int width1, height1, nrChannerls1;
+	unsigned char* data1 = stbi_load("res/images/2.png", &width1, &height1, &nrChannerls1, 0);
+	if (data1)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width1, height1, 0, GL_RGBA, GL_UNSIGNED_BYTE, data1);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Fail to read image" << std::endl;
+	}
+
+
+	stbi_image_free(data1);
+
+
 
 	unsigned int vao;
 	glGenVertexArrays(1, &vao);
@@ -63,26 +111,42 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	unsigned int ibo;
 	glGenBuffers(1, &ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
+	Shader program("res/shaders/basic.shader");
+	program.Use();
+	program.SetInt("texture1", 0);
+	program.SetInt("texture2", 1);
+
+	float increment = 0.05f;
+	float mixVal = 0;
 
 	while (!glfwWindowShouldClose(window))
 	{
 		//清理缓冲区
-		glClearColor(0.4f, 0.2f, 0.3f, 1.0f);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//处理输入
 		PrcocessInput(window);
 		
+		if (mixVal > 1)
+			increment = -0.02f;
+		else if(mixVal < 0)
+			increment = 0.02f;
+
+		mixVal += increment;
+		program.SetFloat("mixVal", mixVal);
 
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
