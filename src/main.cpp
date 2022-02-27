20d4deeda910fbc7443dbff30df25a79f5648606
给贝拉嘉晚饭 <79397301+klsc749@users.noreply.github.com>
2022-02-27 11:15:51 +0800

    Draw a rectangle with 4 colors(red green bule black)

--- src/main.cpp
+++ src/main.cpp
@@ -34,14 +34,15 @@ int main()
 
 	float vertices[] = {
 		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+		 0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
 		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		-0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // top left
+		 0.5f,  -0.5f, 0.0f,  0.0f, 0.0f, 0.0f   // top right
 	};
 
 	unsigned int indices[] = {
 		0, 1, 2,
-		0, 3, 2
+		0, 1, 3
 	};
 
 	Shader program("res/shaders/basic.shader");
@@ -60,10 +61,11 @@ int main()
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(0);
 	glEnableVertexAttribArray(1);
-	//unsigned int ibo;
-	//glGenBuffers(1, &ibo);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
+	unsigned int ibo;
+	glGenBuffers(1, &ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
 
 	while (!glfwWindowShouldClose(window))
@@ -76,7 +78,7 @@ int main()
 		PrcocessInput(window);
 		
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		//Ë«»º´æ½»»»
 		glfwSwapBuffers(window);
