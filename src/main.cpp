32dd24f816353d78b4b5cbf7ce2472529d140ee4
klsc749 <1554208383@qq.com>
2022-03-05 21:22:57 +0800

    Add more cubes

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(900, 900, "Hello OpenGL", NULL, NULL);
+	window = glfwCreateWindow(1200, 900, "Hello OpenGL", NULL, NULL);
 	glfwSwapInterval(2);
 
 	if (!window)
@@ -167,6 +167,18 @@ float vertices[] = {
 
 	glEnable(GL_DEPTH_TEST);
 
+	glm::vec3 cubePositions[] = {
+		  glm::vec3(0.0f,  0.0f,  0.0f),
+		  glm::vec3(2.0f,  5.0f, -15.0f),
+		  glm::vec3(-1.5f, -2.2f, -2.5f),
+		  glm::vec3(-3.8f, -2.0f, -12.3f),
+		  glm::vec3(2.4f, -0.4f, -3.5f),
+		  glm::vec3(-1.7f,  3.0f, -7.5f),
+		  glm::vec3(1.3f, -2.0f, -2.5f),
+		  glm::vec3(1.5f,  2.0f, -2.5f),
+		  glm::vec3(1.5f,  0.2f, -1.5f),
+		  glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 	while (!glfwWindowShouldClose(window))
 	{
 		//清理缓冲区
@@ -194,9 +206,20 @@ float vertices[] = {
 
 		// render box
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			if(i % 3 == 0  || i == 0)
+				model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+			else
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+			glUniformMatrix4fv(glGetUniformLocation(program.id, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		//双缓存交换
 		glfwSwapBuffers(window);
